0b1799076202ae68599280bc4f721a697cb340d5
siarhei.misko <siarhei.misko@viber.com>
2021-08-09 16:51:17 +0300

    test all urls

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
+  console.log(req)
+  res.send('Hello World!')
+})
+
+app.post('/*', (req, res) => {
+  console.log(req)
   res.send('Hello World!')
 })
 

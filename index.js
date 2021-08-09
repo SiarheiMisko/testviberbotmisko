2ec0c61db2cca54fe03146d4a3307eb909f5806a
siarhei.misko <siarhei.misko@viber.com>
2021-08-09 17:25:41 +0300

    test viber bot impl

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/*', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
-})
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
 
-app.post('/*', (req, res) => {
-  console.log(req)
-  res.send('Hello World!')
-})
+const bot = new ViberBot({
+	authToken: "4dcab741f6e7d14c-5a2cc4edf2d9ea8d-63074b3d0a45ec28",
+	name: "Misko Bot",
+	avatar: "",
+});
+
+app.use("/viber/webhook", bot.middleware());
+
+bot.onTextMessage(/^hi|hello$/i, (message, response) =>
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`)));
+
+bot.onError(err => logger.error(err));
+
+bot.onConversationStarted((userProfile, isSubscribed, context, onFinish) =>
+	onFinish(new TextMessage(`Hi, ${userProfile.name}! Nice to meet you.`)));
+
+// app.get('/*', (req, res) => {
+//   console.log(req)
+//   res.send('Hello World!')
+// })
+
+// app.post('/*', (req, res) => {
+//   console.log(req)
+//   res.send('Hello World!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
